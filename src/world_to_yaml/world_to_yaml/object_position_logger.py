d75925d87c952f38fbfd544f8fc008b02e76acf4
Danryg <danryg@student.chalmers.se>
2025-02-05 20:51:37 +0100

    It now only adds points that are inside a specified radius around the robot

--- src/world_to_yaml/world_to_yaml/object_position_logger.py
+++ src/world_to_yaml/world_to_yaml/object_position_logger.py
@@ -1,14 +1,17 @@
+import math
 import os
 
 import rclpy
 import yaml
 from ament_index_python.packages import get_package_share_directory
 from gazebo_msgs.msg import ModelStates
+from nav_msgs.msg import Odometry  # Import Odometry message type
 from rclpy.node import Node
 
 default_location_file = os.path.join(
     get_package_share_directory("tb3_autonomy"), "maps", "sim_house_locations.yaml"
 )
+
 
 class ObjectPositionLogger(Node):
     def __init__(self):
@@ -20,17 +23,45 @@ class ObjectPositionLogger(Node):
             10,
         )
         self.positions = {}
+        self.subscription_2 = self.create_subscription(
+            Odometry,
+            "/odom",
+            self.listener_callback_2,
+            10,
+        )
+        self.tb_position = [0, 0, 0]
+        self.radius = 5
+
+    def listener_callback_2(self, msg):
+        print("Received message")
+        position = msg.pose.pose.position
+        self.tb_position = [position.x, position.y, position.z]
 
     def listener_callback(self, msg):
         print("Received message")
         self.positions = {}
+
         for i, name in enumerate(msg.name):
             position = msg.pose[i].position
-            self.positions[f"location{i + 1}"] = [position.x, position.y, position.z]
+            distance = self.calculate_distance(position, self.tb_position)
+            if distance <= self.radius:  # Only include points within the radius
+                self.positions[f"location{i + 1}"] = [
+                    position.x,
+                    position.y,
+                    position.z,
+                ]
+        print("Pos: ", self.positions)
         self.write_to_yaml()
 
+    def calculate_distance(self, position, tb_position):
+        """Calculates Euclidean distance between two points"""
+        return math.sqrt(
+            (position.x - tb_position[0]) ** 2
+            + (position.y - tb_position[1]) ** 2
+            + (position.z - tb_position[2]) ** 2
+        )
+
     def write_to_yaml(self):
-        print("Writing to YAML file to: ", default_location_file)
         yaml_file = os.path.expanduser(default_location_file)
         with open(yaml_file, "w") as file:
             yaml.dump(self.positions, file, default_flow_style=True)
